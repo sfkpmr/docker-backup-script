45d14fee5f8f8daddc1e303d277e2fa1075268b7
Marcus <sfkpmr@users.noreply.github.com>
2022-04-28 17:36:42 +0200

    Update docker-backup.sh

--- docker-backup.sh
+++ docker-backup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#Docker backup script
+#Docker bind backup script
 
 DATE=$(date '+%Y-%m-%d')
 
 DOCKERCOMPOSEFILE=/docker/docker-compose.yml
-BACKUPUSER=username # A user with permissions to use the backup path 
 BACKUPPATH="/mnt/backups/servers/${HOSTNAME}/"
 BACKUPNAME="${HOSTNAME}-${DATE}.tar.gz"
 
+cd $HOME
+
 #Pause containers, backup and unpause containers
 #The -f option should directly precede the filename. So, use tar -vczf filename.tar.gz instead of -vcfz
-docker-compose -f "${DOCKERCOMPOSEFILE}" pause && tar -zvcpf "$BACKUPNAME" /docker/ && sudo -u "$BACKUPUSER" cp "$BACKUPNAME" "${BACKUPPATH}" && rm -f "$BACKUPNAME"
+docker-compose -f "${DOCKERCOMPOSEFILE}" pause && tar -zvcpf "$BACKUPNAME" /docker/ && cp ${BACKUPNAME} ${BACKUPPATH} && rm -f "$BACKUPNAME"
 docker-compose -f "${DOCKERCOMPOSEFILE}" unpause
